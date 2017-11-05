cce5438ae9c7865a6ced2cb8f1653adf9d93cb98
phinocio <github@phinocio.com>
2017-11-04 23:00:13 -0700

    Finished generating required mods

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+use Sunra\PhpSimple\HtmlDomParser;
+
+$html = HtmlDomParser::file_get_html('sources/us-mods.html');
+
+$mods = $html->find('td[bgcolor=#000000]');
+
+for($i=0; $i<count($mods);)
+{	
+	$requiredMods[$i] = [
+		'name' => trim($mods[$i]->parent()->children(2)->first_child()->plaintext),
+		'url' => substr($mods[$i]->parent()->children(2)->first_child()->first_child()->next_sibling()->href, 0, strrpos($mods[$i]->parent()->children(2)->first_child()->first_child()->next_sibling()->href, '/')),
+		'author' => trim(str_replace(' by: ', '', $mods[$i]->parent()->children(2)->first_child()->next_sibling()->plaintext))
+	]; 
+    $i++; 
+}
+
+echo json_encode($requiredMods);
